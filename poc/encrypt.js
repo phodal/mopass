dc489786f9fab256103e9e40130e943a3c4d4242
Phodal HUANG <h@phodal.com>
2019-02-18 07:54:37 +0800

    poc: init ecrypt decrypt

--- poc/encrypt.js
+++ poc/encrypt.js
@@ -1,5 +1,21 @@
-var CryptoJS = require("crypto-js");
-console.log(CryptoJS.HmacSHA1("Message", "Key"));
-console.log(CryptoJS.SHA256("Message"));
-console.log(CryptoJS.MD5("Message"));
-console.log(CryptoJS.HmacMD5("Message", "Key"));
+const CryptoJS = require('crypto-js');  //引用AES源码js
+
+const key = CryptoJS.enc.Utf8.parse("1234123412ABCDEF");  // 十六位十六进制数作为密钥
+const iv = CryptoJS.enc.Utf8.parse('ABC-DFA');   // 十六位十六进制数作为密钥偏移量
+
+function decrypt(word) {
+  let encryptedHexStr = CryptoJS.enc.Hex.parse(word);
+  let srcs = CryptoJS.enc.Base64.stringify(encryptedHexStr);
+  let decrypt = CryptoJS.AES.decrypt(srcs, key, { iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 });
+  let decryptedStr = decrypt.toString(CryptoJS.enc.Utf8);
+  return decryptedStr.toString();
+}
+
+function encrypt(word) {
+  let srcs = CryptoJS.enc.Utf8.parse(word);
+  let encrypted = CryptoJS.AES.encrypt(srcs, key, { iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 });
+  return encrypted.ciphertext.toString().toUpperCase();
+}
+
+console.log(encrypt('phodal'));
+console.log(decrypt('CBE4BEFD00FE16653E51FE61AACA2BBE'));
