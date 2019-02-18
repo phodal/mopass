2f7e2f507f5fec1a7c10c02e2ad30bef759e0fab
Phodal HUANG <h@phodal.com>
2019-02-18 12:39:54 +0800

    server: init

---
+++ server/src/home.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+};
--- server/src/vertify.js
+++ server/src/vertify.js
@@ -8,7 +8,4 @@ module.exports.hello = async (event, context) => {
       input: event,
     }),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
