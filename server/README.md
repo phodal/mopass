76799cc104fed7ff7fdb8ee23a508a9a6e4848d8
Phodal HUANG <h@phodal.com>
2019-02-18 21:06:28 +0800

    server: add basic get services

---
+++ server/README.md
@@ -0,0 +1,7 @@
+# Serverless Password Manager
+
+Test CLI:
+
+```
+curl --header "Authorization: allow" -X POST -H "Content-type: application/json" -d '{"a": 2222225}' https://spm.wdsm.io/sync
+```
--- server/src/sync.js
+++ server/src/sync.js
@@ -1,4 +1,11 @@
 'use strict';
+const shortid = require('shortid');
+const AWS = require('aws-sdk');
+
+AWS.config.setPromisesDependency(Promise);
+
+const tableName = process.env.DYNAMODB_TABLE;
+const docClient = new AWS.DynamoDB.DocumentClient();
 
 async function deleteItem(event, context, callback) {
   return {
@@ -28,12 +35,45 @@ async function patchItem(event, context, callback) {
 }
 
 async function postItem(event, context, callback) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      method: 'postItem'
-    }),
+  let body;
+  try {
+    body = JSON.parse(event.body);
+  } catch (error) {
+    console.log(error);
+    callback(null, {statusCode: 400, body: JSON.stringify(error)});
+  }
+  console.log(body);
+
+  const timestamp = new Date().getTime();
+  const params = {
+    TableName: tableName,
+    Item: {
+      password: body.password,
+      id: shortid.generate(),
+      createdAt: timestamp
+    }
   };
+
+  docClient.put(params, (error) => {
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: {'Content-Type': 'text/plain'},
+        body: 'couldn\'t create the form item.',
+      });
+      return;
+    }
+
+    const response = {
+      statusCode: 201,
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      },
+      body: JSON.stringify(params.Item),
+    };
+    callback(null, response);
+  });
 }
 
 async function updateItem(event, context, callback) {
