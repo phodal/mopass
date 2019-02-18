81293a57a3676a0a3be09265e320fd15b02dba81
Phodal HUANG <h@phodal.com>
2019-02-18 16:26:57 +0800

    server: update auth

--- server/src/auth.js
+++ server/src/auth.js
@@ -1,11 +1,40 @@
 'use strict';
 
-module.exports.authorize = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+const generatePolicy = function(principalId, effect, resource) {
+  const authResponse = {};
+  authResponse.principalId = principalId;
+  if (effect && resource) {
+    const policyDocument = {};
+    policyDocument.Version = '2012-10-17';
+    policyDocument.Statement = [];
+    const statementOne = {};
+    statementOne.Action = 'execute-api:Invoke';
+    statementOne.Effect = effect;
+    statementOne.Resource = resource;
+    policyDocument.Statement[0] = statementOne;
+    authResponse.policyDocument = policyDocument;
+  }
+  return authResponse;
+};
+
+module.exports.authorize = (event, context, callback) => {
+  console.log(event);
+  console.log("==================");
+  console.log("Authorization: ", event.authorizationToken);
+  console.log("==================");
+  var token = event.authorizationToken;
+
+  switch (token) {
+    case 'allow':
+      callback(null, generatePolicy('user', 'Allow', event.methodArn));
+      break;
+    case 'deny':
+      callback(null, generatePolicy('user', 'Deny', event.methodArn));
+      break;
+    case 'unauthorized':
+      callback('Unauthorized');
+      break;
+    default:
+      callback('Error');
+  }
 };
