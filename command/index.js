42f53a0f823c21a6384fbdbb02b8e942529f6954
Phodal HUANG <h@phodal.com>
2019-02-20 11:00:10 +0800

    feat: [CLI] add basic inquirer

--- command/index.js
+++ command/index.js
@@ -1,8 +1,31 @@
 #!/usr/bin/env node
-
+const inquirer = require('inquirer');
 const argv = require('yargs')
-  .usage('Usage: $0 -w [num] -h [num]')
-  .demandOption(['w', 'h'])
+  .usage('Usage: $0 [commmand]')
   .argv;
 
-console.log("The area is:", argv.w * argv.h);
+console.log(argv.generate);
+if (argv.generate) {
+  var validateLength = function (data) {
+    console.log(data);
+    return /\d/.test(data);
+  };
+
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'length',
+        message: 'password length',
+        validate: validateLength
+      },
+      {
+        type: 'list',
+        name: 'type',
+        message: 'choice password type',
+        choices: ['secure', 'normal']
+      }])
+    .then(answers => {
+      console.log(answers);
+    });
+}
