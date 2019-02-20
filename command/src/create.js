3224eeafc9f803b9d5e48e6b720f25266a5850b0
Phodal HUANG <h@phodal.com>
2019-02-20 21:09:30 +0800

    feat: [CLI] add double copy for steps

--- command/src/create.js
+++ command/src/create.js
@@ -2,30 +2,36 @@
 
 const inquirer = require('inquirer');
 const dbm = require('./dbm');
+const { from } = require('rxjs');
 
 function validatePassword(data) {
   return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}/.test(data);
 }
 
 function creator() {
-  inquirer
-    .prompt([
-      {
-        type: 'input',
-        name: 'length',
-        message: 'password title',
-        validate: dbm.checkTitleDuplicate
-      },
-      {
-        type: 'password',
-        name: 'length',
-        mask: true,
-        message: 'your password',
-        validate: validatePassword
-      }
-    ])
-    .then(answers => {
-      console.log(answers);
+  const questions = [{
+    type: 'input',
+    name: 'length',
+    message: 'password title',
+    validate: dbm.checkTitleDuplicate
+  }, {
+    type: 'password',
+    name: 'length',
+    mask: true,
+    message: 'your password',
+    validate: validatePassword
+  }];
+
+  const observable = from(questions);
+  inquirer.prompt(observable).ui.process.subscribe(
+    function(ans) {
+      console.log('Answer is: ', ans);
+    },
+    function(err) {
+      console.log('Error: ', err);
+    },
+    function() {
+      console.log('Completed');
     });
 }
 
