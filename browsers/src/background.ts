fede70bccc80ee3b64e53687561b06a68c060a63
Phodal HUANG <h@phodal.com>
2019-02-22 17:48:30 +0800

    feat: [browsers] add basic promise

--- browsers/src/background.ts
+++ browsers/src/background.ts
@@ -1,10 +1,20 @@
 import Mopass from 'mopass-common';
 
-function polling() {
-  Mopass.Fetch.fetchPasswordsPromise().then((data) => {
-    console.log(data);
-  });
+const mopassKey = localStorage.getItem('mopass.key')
+const password = localStorage.getItem('mopass.test')
+const hashString = Mopass.EncryptUtil.hashString(password);
 
+Mopass.EncryptUtil.configIv({iv: hashString});
+Mopass.TokenManager.setUserToken(hashString);
+
+Mopass.Fetch.fetchPasswordsPromise().then((response) => {
+  let firstPassword = response.data.Items[0].password
+  console.log(firstPassword, mopassKey);
+  var result = Mopass.EncryptUtil.decrypt(firstPassword, mopassKey);
+  console.log(result);
+});
+
+function polling() {
   setTimeout(polling, 1000 * 30);
 }
 
