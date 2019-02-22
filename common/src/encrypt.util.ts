62742991e82e7e482b35d4ff12cca6be7ad96d01
Phodal HUANG <h@phodal.com>
2019-02-22 14:16:25 +0800

    refactor: remove unused key

--- common/src/encrypt.util.ts
+++ common/src/encrypt.util.ts
@@ -11,36 +11,31 @@ function isExistKeyAndIv(key: string, iv: string) {
   return true;
 }
 
-function decrypt(word: string) {
-  if (!isExistKeyAndIv(readKey(), iv)) {
+function decrypt(word: string, key: any) {
+  if (!isExistKeyAndIv(key, iv)) {
     return;
   }
 
   let encryptedHexStr = CryptoJS.enc.Hex.parse(word);
   let srcs = CryptoJS.enc.Base64.stringify(encryptedHexStr);
-  let decrypt = CryptoJS.AES.decrypt(srcs, readKey(), {iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7});
+  let decrypt = CryptoJS.AES.decrypt(srcs, key, {iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7});
   let decryptedStr = decrypt.toString(CryptoJS.enc.Utf8);
   return decryptedStr.toString();
 }
 
-function encrypt(word: any) {
-  if (!isExistKeyAndIv(readKey(), iv)) {
+function encrypt(word: any, key: any) {
+  if (!isExistKeyAndIv(key, iv)) {
     return;
   }
 
   let srcs = CryptoJS.enc.Utf8.parse(word);
-  let encrypted = CryptoJS.AES.encrypt(srcs, readKey(), {iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7});
+  let encrypted = CryptoJS.AES.encrypt(srcs, key, {iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7});
   return encrypted.ciphertext.toString().toUpperCase();
 }
 
 function configIv(config: any) {
   iv = CryptoJS.enc.Utf8.parse(config.iv);
 }
-
-function readKey() {
-  return ""
-}
-
 
 module.exports.decrypt = decrypt;
 module.exports.encrypt = encrypt;
