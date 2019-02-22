2180213f6a9d837128a7feb62932c9d0a44122c0
Phodal HUANG <h@phodal.com>
2019-02-22 12:42:38 +0800

    browsers: change language to typescripts

---
+++ browsers/webpack/webpack.common.js
@@ -0,0 +1,37 @@
+const webpack = require("webpack");
+const path = require('path');
+
+module.exports = {
+  entry: {
+    popup: path.join(__dirname, '../src/popup.ts'),
+    options: path.join(__dirname, '../src/options.ts'),
+    background: path.join(__dirname, '../src/background.ts'),
+    content_script: path.join(__dirname, '../src/content_script.ts')
+  },
+  output: {
+    path: path.join(__dirname, '../dist/js'),
+    filename: '[name].js'
+  },
+  optimization: {
+    splitChunks: {
+      name: 'vendor',
+      chunks: "initial"
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  plugins: [
+    // exclude locale files in moment
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+  ]
+};
