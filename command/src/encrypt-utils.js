7c1fbffcbda0b7004dc61adc58d3cb6f7bbf3049
Phodal HUANG <h@phodal.com>
2019-02-25 07:35:39 +0800

    fix: fix key issue

--- command/src/encrypt-utils.js
+++ command/src/encrypt-utils.js
@@ -5,8 +5,10 @@ const generator = require('./generator')
 
 let iv
 
-let key = CryptoJS.enc.Utf8.parse(fs.readFileSync(__dirname + '/.mopass.key').toString())
-console.log(fs.readFileSync(__dirname + '/.mopass.key').toString())
+function getKey() {
+  let getKey = fs.readFileSync(__dirname + '/.mopass.key').toString().split('\n')[0]
+  return CryptoJS.enc.Utf8.parse(getKey)
+}
 
 function isExistKeyAndIv(key, iv) {
   if (!key || !iv) {
@@ -18,24 +20,24 @@ function isExistKeyAndIv(key, iv) {
 }
 
 function decrypt(word) {
-  if (!isExistKeyAndIv(key, iv)) {
+  if (!isExistKeyAndIv(getKey(), iv)) {
     return
   }
 
   let encryptedHexStr = CryptoJS.enc.Hex.parse(word)
   let srcs = CryptoJS.enc.Base64.stringify(encryptedHexStr)
-  let decrypt = CryptoJS.AES.decrypt(srcs, key, { iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 })
+  let decrypt = CryptoJS.AES.decrypt(srcs, getKey(), { iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 })
   let decryptedStr = decrypt.toString(CryptoJS.enc.Utf8)
   return decryptedStr.toString()
 }
 
 function encrypt(word) {
-  if (!isExistKeyAndIv(key, iv)) {
+  if (!isExistKeyAndIv(getKey(), iv)) {
     return
   }
 
   let srcs = CryptoJS.enc.Utf8.parse(word);
-  let encrypted = CryptoJS.AES.encrypt(srcs, key, {iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7});
+  let encrypted = CryptoJS.AES.encrypt(srcs, getKey(), {iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7});
   return encrypted.ciphertext.toString().toUpperCase();
 }
 
