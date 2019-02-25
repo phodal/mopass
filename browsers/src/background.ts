dbb8cf4b69896b97b98f9e44164854032b33bd6b
Phodal HUANG <h@phodal.com>
2019-02-25 16:52:25 +0800

    fix: change events

--- browsers/src/background.ts
+++ browsers/src/background.ts
@@ -1,25 +1,23 @@
 import Mopass from 'mopass-common'
 
-let mopassKey
-const password = localStorage.getItem('mopass.test')
-const hashString = Mopass.EncryptUtil.hashString(password)
+function fetchPasswordsInBackground(password, callback) {
+  let mopassKey
+  const hashString = Mopass.EncryptUtil.hashString(password)
 
-chrome.storage.sync.get({
-  mopassKey: ''
-}, function(items: { mopassKey }) {
-  mopassKey = Mopass.EncryptUtil.encParse(items.mopassKey)
-})
-
-Mopass.EncryptUtil.configIv({ iv: password })
-Mopass.TokenManager.setUserToken(hashString)
-
-Mopass.Fetch.fetchPasswordsPromise().then((response) => {
-  savePasswords(response.data.Items)
-  getPasswordByTitle('ji', function(passwordItem) {
-    const password = Mopass.EncryptUtil.decrypt(passwordItem.password, mopassKey)
-    console.log(password)
+  chrome.storage.sync.get({
+    mopassKey: ''
+  }, function(items: { mopassKey }) {
+    mopassKey = Mopass.EncryptUtil.encParse(items.mopassKey)
   })
-})
+
+  Mopass.EncryptUtil.configIv({ iv: password })
+  Mopass.TokenManager.setUserToken(hashString)
+
+  Mopass.Fetch.fetchPasswordsPromise().then((response) => {
+    savePasswords(response.data.Items)
+    callback();
+  })
+}
 
 function savePasswords(passwords: any) {
   chrome.storage.sync.set({
--- browsers/src/popup.ts
+++ browsers/src/popup.ts
@@ -1,29 +1,33 @@
 import * as $ from 'jquery'
 
-function showAllPasswordsTitle() {
+function showAllPasswordsTitle(pwd) {
   let $password = $('#page-password')
-
   $password.html('<div>loading</div>')
-  let domStr = '<div>'
-  chrome.storage.sync.get(['passwords'], function(items) {
-    console.log(items);
-    for (let i = 0; i < items.passwords.length; i++) {
-      const passwordItem = items.passwords[i]
-      domStr += `<p id="password-${i}">${passwordItem.title}</p>`
-    }
 
-    domStr += '<div>'
-    $password.html(domStr)
-    $password.show();
+  chrome.runtime.getBackgroundPage(function(bgWindow: any) {
+    bgWindow.fetchPasswordsInBackground(pwd, function() {
+      let domStr = '<div>'
+      chrome.storage.sync.get(['passwords'], function(items) {
+        console.log(items)
+        for (let i = 0; i < items.passwords.length; i++) {
+          const passwordItem = items.passwords[i]
+          domStr += `<p id="password-${i}">${passwordItem.title}</p>`
+        }
+
+        domStr += '<div>'
+        $password.html(domStr)
+        $password.show()
+      })
+    })
   })
 }
 
 $(function() {
-  console.log('showAllPasswordsTitle');
+  console.log('showAllPasswordsTitle')
   $('#master-password').submit(function(e) {
-    console.log(e);
     e.preventDefault()
-    $('#page-master').hide();
-    showAllPasswordsTitle()
+    $('#page-master').hide()
+    console.log($('#password').val())
+    showAllPasswordsTitle($('#password').val())
   })
 })
