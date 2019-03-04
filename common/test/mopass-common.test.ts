373b2857924c405a8706fc4e4ca56a7e76feba0e
Phodal HUANG <h@phodal.com>
2019-03-05 07:41:54 +0800

    test: add more test

--- common/test/mopass-common.test.ts
+++ common/test/mopass-common.test.ts
@@ -1,7 +1,8 @@
 const CryptoJS = require('crypto-js')
 import EncryptUtil from '../src/encrypt.util'
+import TokenManager from '../src/token-manager'
 
-describe('Dummy test', () => {
+describe('Encrypt test', () => {
   let encryptKey: any
   beforeEach(function() {
     EncryptUtil.configIv('master word')
@@ -14,5 +15,18 @@ describe('Dummy test', () => {
     let decryptWord = EncryptUtil.decrypt(encryptWord, encryptKey)
 
     expect(decryptWord).toEqual(word)
+  })
+
+  it('should enable hash string', () => {
+    let hashString = EncryptUtil.hashString('phodal')
+    expect(hashString).toEqual('a1eb5e02c050ae0e0bcfe8354df35fc0')
+  })
+})
+
+describe('Token Manager', () => {
+  it('should get/set token', () => {
+    let token = EncryptUtil.hashString('phodal')
+    TokenManager.setUserToken(token)
+    expect(TokenManager.getUserToken()).toEqual(token)
   })
 })
