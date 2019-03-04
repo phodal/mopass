a030c81cadc9cc28c2f9dd6984422d0233bafa10
Phodal HUANG <h@phodal.com>
2019-03-05 07:33:58 +0800

    test: add first test

--- common/test/mopass-common.test.ts
+++ common/test/mopass-common.test.ts
@@ -1,5 +1,18 @@
-describe("Dummy test", () => {
-  it("works if true is truthy", () => {
-    expect(true).toBeTruthy()
+const CryptoJS = require('crypto-js')
+import EncryptUtil from '../src/encrypt.util'
+
+describe('Dummy test', () => {
+  let encryptKey: any
+  beforeEach(function() {
+    EncryptUtil.configIv('master word')
+    encryptKey = CryptoJS.enc.Utf8.parse('80fe4087-b8da-432a-b003-028667625b7b')
+  })
+
+  it('should encrypt and decrypt to same words', () => {
+    let word = 'phodal'
+    let encryptWord = EncryptUtil.encrypt(word, encryptKey)
+    let decryptWord = EncryptUtil.decrypt(encryptWord, encryptKey)
+
+    expect(decryptWord).toEqual(word)
   })
 })
