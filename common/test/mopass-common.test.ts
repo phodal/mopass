9818526f3d6d022c87f68034d91921c89bcc1918
Phodal HUANG <h@phodal.com>
2019-03-05 07:25:24 +0800

    feat: thinking in test

--- common/test/mopass-common.test.ts
+++ common/test/mopass-common.test.ts
@@ -1,14 +1,5 @@
-import DummyClass from "../src/generator"
-
-/**
- * Dummy test
- */
 describe("Dummy test", () => {
   it("works if true is truthy", () => {
     expect(true).toBeTruthy()
-  })
-
-  it("DummyClass is instantiable", () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
   })
 })
